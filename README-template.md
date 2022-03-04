dd888339b7995586986a73e16e15fb05c7c06cb4
DavinciCode <emsthias33@gmail.com>
2022-03-04 18:13:44 +0100

    jackie

---
+++ README-template.md
@@ -0,0 +1,37 @@
+# Frontend Mentor - Launch countdown timer solution
+
+This is a solution to the [Launch countdown timer challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/launch-countdown-timer-N0XkGfyz-). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+- [My process](#my-process)
+  - [Built with](#built-with)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- See hover states for all interactive elements on the page
+- See a live countdown timer that ticks down every second (start the count at 14 days)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+- [React](https://reactjs.org/) - JS library
+
+## Author
+
+- Website - [Add your name here](https://www.your-site.com)
+- Frontend Mentor - [@yourusername](https://www.frontendmentor.io/profile/RoyDavinci)
+- Twitter - [@yourusername](https://twitter.com/roydavinci5)
--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect, useCallback } from "react";
+import Facebook from "./images/icon-facebook.svg";
+import Instagram from "./images/icon-instagram.svg";
+import Pintrest from "./images/icon-pinterest.svg";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [day, setDay] = useState("");
+	const [hours, setHours] = useState("");
+	const [minutes, setMinutes] = useState("");
+	const [seconds, setSeconds] = useState("");
+	const [finish, setFinish] = useState("");
+	const [time, setTime] = useState("");
+	const [show, setShow] = useState(false);
+
+	let futureDate = new Date(2022, 2, 12, 10, 59, 45);
+	let futurTime = futureDate.getTime();
+
+	const getRemainingTime = useCallback(() => {
+		const today = new Date().getTime();
+		const realTime = futurTime - today;
+		let oneDay = 24 * 60 * 60 * 1000;
+		let oneHour = 60 * 60 * 1000;
+		let oneMinute = 60 * 1000;
+
+		let days = Math.floor(realTime / oneDay);
+		let hours = Math.floor((realTime % oneDay) / oneHour);
+		let minutes = Math.floor((realTime % oneHour) / oneMinute);
+		let seconds = Math.floor((realTime % oneMinute) / 1000);
+
+		function format(item) {
+			if (item < 10) {
+				return (item = `0${item}`);
+			}
+			return item;
+		}
+		// set values array
+
+		const values = [days, hours, minutes, seconds];
+		setDay(format(values[0]));
+		setHours(format(values[1]));
+		setMinutes(format(values[2]));
+		setSeconds(format(values[3]));
+		setTime(realTime);
+	}, [futurTime]);
+
+	let countDown = setInterval(getRemainingTime, 1000);
+	if (time < 0) {
+		clearInterval(countDown);
+		setShow(true);
+		setFinish("Sorry this giveaway has expired");
+	}
+
+	useEffect(() => {
+		getRemainingTime();
+	}, [getRemainingTime]);
+
+	return (
+		<div className='App'>
+			<div className='top'>
+				<div className='content'>
+					{show ? (
+						<h1>{finish}</h1>
+					) : (
+						<>
+							<h1>WE ARE LAUNCHING SOON</h1>
+							<div className='time'>
+								<div className='days'>
+									<h1>{day}</h1>
+									<p>DAYS</p>
+								</div>
+								<div className='hours'>
+									<h1>{hours}</h1>
+									<p>HOURS</p>
+								</div>
+								<div className='minutes'>
+									<h1>{minutes}</h1>
+									<p>MINUTES</p>
+								</div>
+								<div className='seconds'>
+									<h1>{seconds}</h1>
+									<p>SECONDS</p>
+								</div>
+							</div>
+						</>
+					)}
+				</div>
+			</div>
+
+			<div className='bottom'>
+				<div className='images'>
+					<img src={Facebook} alt='' />
+					<img src={Instagram} alt='' />
+					<img src={Pintrest} alt='' />
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
